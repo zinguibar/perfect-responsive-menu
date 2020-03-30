502bb26a4841155488264ae88a8a44344cc0ad79
zinguibar <e.zhassani@laposte.net>
2020-03-30 14:33:51 +0200

    Modify files

--- navigation.js
+++ navigation.js
@@ -5,4 +5,8 @@ toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
+
+	if (menu.hidden == false) {
+		body.classList.add("no-noscroll");
+	}
 });
